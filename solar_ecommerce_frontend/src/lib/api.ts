6cff9699436bbef87a30ebfa144ce233aeb44504
Paras Ghimire <iamparasghimire@gmail.com>
2026-03-20 13:18:49 +0545

    add

--- solar_ecommerce_frontend/src/lib/api.ts
+++ solar_ecommerce_frontend/src/lib/api.ts
@@ -1,6 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "") ||
-  "http://127.0.0.1:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL?.replace(/\/+$/, "");
 
 type Primitive = string | number | boolean;
 
